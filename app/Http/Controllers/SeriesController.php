3f7f4601a98b39cce7a6f27eed47d11e77b64f2a
Gilberto Barbosa <gisantos555@gmail.com>
2022-09-18 12:26:40 -0300

    Capítulos 3 e 4

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -15,12 +15,11 @@ class SeriesController extends Controller
             'Isto é bolo?'
         ];
 
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
+        return view('series.index')->with('series', $series);
+    }
 
-        return $html;
+    public function create()
+    {
+        return view('series.create');
     }
 }
