54f8d88b724073230ee35c3fcee25ba2dc8745ce
Gilberto Barbosa <gisantos555@gmail.com>
2022-09-18 23:55:30 -0300

    Capítulo 5

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         
-        $series = [
-            'A pior das Bruxas',
-            'Truques da Mentes',
-            'Isto é bolo?'
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
 
         return view('series.index')->with('series', $series);
     }
@@ -21,5 +19,16 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        
+        return redirect('/series');
+
     }
 }
